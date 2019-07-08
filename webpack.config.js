39899e1750fcf7c96dc4af6154d3226ddf3715ef
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-07-08 14:19:21 +1200

    webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,17 @@ module.exports = {
       path: __dirname + '/public',
       filename: 'bundle.js'
     },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devtool: 'source-map'
+    // module: {
+    //   loaders: [
+    //     {
+    //       test: /\.jsx?$/,
+    //       loader: 'babel-loader',
+    //       exclude: /node_modules/
+    //     },
+    //   ]
+    // },
+    // resolve: {
+    //   extensions: ['.js', '.jsx']
+    // },
+    // devtool: 'source-map'
   }
